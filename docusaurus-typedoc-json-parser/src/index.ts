43e5137375cbc99abaf8d12422c1844f21eeee2d
Jeroen Claassens <support@favware.tech>
2022-10-23 16:57:17 +0200

    fix(typedoc-parser): fixed no auth requests

--- docusaurus-typedoc-json-parser/src/index.ts
+++ docusaurus-typedoc-json-parser/src/index.ts
@@ -25,43 +25,34 @@ export default function docusaurusTypeDocJsonParser(context: LoadContext, option
 			const { siteDir } = context;
 			const { githubContentUrl, githubToken } = options;
 
-			const contents = (
-				await (githubToken
-					? fetch<RepositoryContent[]>(githubContentUrl, {
-							headers: {
-								Authorization: `Bearer ${githubToken}`
-							}
-					  })
-					: fetch<RepositoryContent[]>(githubContentUrl))
-			).filter((content) => content.type === RepositoryContentFileType.Directory);
+			const headers = new Headers();
 
-			for (const content of contents) {
-				const projectContents = await fetch<RepositoryContent[]>(content.url, {
-					headers: {
-						Authorization: `Bearer ${githubToken}`
-					}
-				});
+			if (githubToken) {
+				headers.append('Authorization', `Bearer ${githubToken}`);
+			}
 
-				for (const projectContent of projectContents) {
-					if (projectContent.type === RepositoryContentFileType.Directory) {
-						const projectSubContents = await fetch<RepositoryContent[]>(projectContent.url, {
-							headers: {
-								Authorization: `Bearer ${githubToken}`
-							}
-						});
+			const repositoryContents = await fetch<RepositoryContent[]>(githubContentUrl, { headers });
+			const repositoryDirectories = repositoryContents.filter((content) => content.type === RepositoryContentFileType.Directory);
 
-						writeCategoryYaml(resolve(siteDir, 'docs', 'Documentation', content.name), '', content.name, 0);
+			for (const directory of repositoryDirectories) {
+				const directoryContents = await fetch<RepositoryContent[]>(directory.url, { headers });
 
-						for (const projectSubContent of projectSubContents) {
-							if (projectSubContent.download_url === null)
+				for (const directoryContent of directoryContents) {
+					if (directoryContent.type === RepositoryContentFileType.Directory) {
+						const subDirectoryContents = await fetch<RepositoryContent[]>(directoryContent.url, { headers });
+
+						writeCategoryYaml(resolve(siteDir, 'docs', 'Documentation', directory.name), '', directory.name, 0);
+
+						for (const subDirectoryContent of subDirectoryContents) {
+							if (subDirectoryContent.download_url === null)
 								throw new Error(
-									`The 'download_url' field is null for '${content.name}/${projectContent.name}/${projectSubContent.name}'`
+									`The 'download_url' field is null for '${directory.name}/${directoryContent.name}/${subDirectoryContent.name}'`
 								);
 
 							writeCategoryYaml(
-								resolve(siteDir, 'docs', 'Documentation', content.name, projectContent.name),
+								resolve(siteDir, 'docs', 'Documentation', directory.name, directoryContent.name),
 								'',
-								projectContent.name,
+								directoryContent.name,
 								1
 							);
 
@@ -69,44 +60,44 @@ export default function docusaurusTypeDocJsonParser(context: LoadContext, option
 								siteDir,
 								'docs',
 								'Documentation',
-								content.name,
-								projectContent.name,
-								projectSubContent.name.replace('.json', '')
+								directory.name,
+								directoryContent.name,
+								subDirectoryContent.name.replace('.json', '')
 							);
 
 							removeDirectory(outputDir);
 
-							writeCategoryYaml(outputDir, '', projectSubContent.name.replace('.json', ''), 1);
+							writeCategoryYaml(outputDir, '', subDirectoryContent.name.replace('.json', ''), 1);
 
-							const data = await fetch<ProjectParser.JSON>(projectSubContent.download_url);
+							const data = await fetch<ProjectParser.JSON>(subDirectoryContent.download_url);
 							const incomingTypeDocJsonParserVersion = data.typeDocJsonParserVersion.split('.').map(Number) as [number, number, number];
 							const currentTypeDocJsonParserVersion = ProjectParser.version.split('.').map(Number) as [number, number, number];
 
 							if (incomingTypeDocJsonParserVersion[0] !== currentTypeDocJsonParserVersion[0]) continue;
 
-							const projectParser = new ProjectParser({ data, version: projectSubContent.name.replace('.json', '') });
+							const projectParser = new ProjectParser({ data, version: subDirectoryContent.name.replace('.json', '') });
 
 							renderOutputFiles(projectParser, outputDir, true);
 						}
 					} else {
-						if (projectContent.download_url === null)
-							throw new Error(`The 'download_url' field is null for '${content.name}/${projectContent.name}'`);
+						if (directoryContent.download_url === null)
+							throw new Error(`The 'download_url' field is null for '${directory.name}/${directoryContent.name}'`);
 
-						writeCategoryYaml(resolve(siteDir, 'docs', 'Documentation', content.name), '', content.name, 0);
+						writeCategoryYaml(resolve(siteDir, 'docs', 'Documentation', directory.name), '', directory.name, 0);
 
-						const outputDir = resolve(siteDir, 'docs', 'Documentation', content.name, projectContent.name.replace('.json', ''));
+						const outputDir = resolve(siteDir, 'docs', 'Documentation', directory.name, directoryContent.name.replace('.json', ''));
 
 						removeDirectory(outputDir);
 
-						writeCategoryYaml(outputDir, '', projectContent.name.replace('.json', ''), 1);
+						writeCategoryYaml(outputDir, '', directoryContent.name.replace('.json', ''), 1);
 
-						const data = await fetch<ProjectParser.JSON>(projectContent.download_url);
+						const data = await fetch<ProjectParser.JSON>(directoryContent.download_url);
 						const incomingTypeDocJsonParserVersion = data.typeDocJsonParserVersion.split('.').map(Number) as [number, number, number];
 						const currentTypeDocJsonParserVersion = ProjectParser.version.split('.').map(Number) as [number, number, number];
 
 						if (incomingTypeDocJsonParserVersion[0] !== currentTypeDocJsonParserVersion[0]) continue;
 
-						const projectParser = new ProjectParser({ data, version: projectContent.name.replace('.json', '') });
+						const projectParser = new ProjectParser({ data, version: directoryContent.name.replace('.json', '') });
 
 						renderOutputFiles(projectParser, outputDir, false);
 					}
