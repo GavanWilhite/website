5e0dc9e3862bc21e13c2be3253d6d5911e01c0e2
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2022-10-27 13:10:34 -0400

    fix(deps): update dependency typedoc-json-parser to ^7.0.1

--- json-parser/src/cli.ts
+++ json-parser/src/cli.ts
@@ -2,13 +2,10 @@
 
 import { docusaurusTypeDocJsonParser } from './parser';
 import { config } from 'dotenv';
-import { URLSearchParams } from 'node:url';
 
 config();
 
 await docusaurusTypeDocJsonParser({
-	githubContentUrl: `https://api.github.com/repos/sapphiredev/docs/contents/docs?${new URLSearchParams({
-		ref: 'migration/2022-10-26-20-23'
-	})}`,
+	githubContentUrl: `https://api.github.com/repos/sapphiredev/docs/contents/docs`,
 	githubToken: process.env.GITHUB_TOKEN
 });
